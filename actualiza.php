ad228c3ac20d831f164f9fa423332b0d2cdf64c4
kjmoralescorhuila <kjmorales@corhuila.edu.co>
2017-11-30 12:06:50 -0500

    Actualiar contacto

---
+++ actualiza.php
@@ -0,0 +1,26 @@
+<?php
+final class Actualizar {
+
+	public static function main(){
+		session_start();
+
+		$mysqli = new mysqli("localhost", "root", "", "bdpersona");	
+	
+		$id = $_POST['id'];
+		$nombre = $_POST['nombre'];
+		$edad =  $_POST['edad'];
+		$direccion =  $_POST['direccion'];	
+
+		$sql = $mysqli->query("update tbcontactos set nombre='$nombre', edad=$edad, direccion='$direccion' where id=$id");
+	}
+
+}
+Actualizar::main();
+?>	
+
+	 <SCRIPT LANGUAGE="javascript"> 
+         alert("Contacto Actualizado"); 
+     </SCRIPT> 
+     <META HTTP-EQUIV="Refresh" CONTENT="0; URL=listar.php">
+
+
